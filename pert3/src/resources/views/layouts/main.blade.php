3ab7de9b2c0f3572c13cd7c3b3b47ef7b61d3787
root <root@teddy-omen15>
2023-05-09 18:03:50 +0700

    add image html

---
+++ pert3/src/resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+      @include('partials.header')
+      @include('partials.footer')
+      @include('partials.scripts')
+      <h1>Teddy Adi Kusuma</h1>
+      <h1>20210801149</h1>
+      <img src="https://i.kym-cdn.com/entries/icons/original/000/012/165/Heavy_and_Scout_think_they_are_birds_0-3_screenshot.png" class="img-thumbnail" alt="...">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
